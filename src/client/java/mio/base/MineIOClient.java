3131f2fdca505ba29572f809a35ddbb2508a8c43
Jan Barfuß <janabr2001@gmail.com>
2023-10-27 00:05:57 +0200

    init commit

---
+++ src/client/java/mio/base/MineIOClient.java
@@ -0,0 +1,10 @@
+package mio.base;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineIOClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
