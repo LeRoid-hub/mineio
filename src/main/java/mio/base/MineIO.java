4a5c6159d9d2911cfa17e2c5736b0442c24fee1f
Jan Barfuß <janabr2001@gmail.com>
2023-10-27 13:06:26 +0200

    fixed imports

--- src/main/java/mio/base/MineIO.java
+++ src/main/java/mio/base/MineIO.java
@@ -1,8 +1,14 @@
 package mio.base;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.api.Block;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +17,7 @@ public class MineIO implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("mineio");
-    public static final Block MUD_FURNACE = new Block(FabricBlockSettings.create().strenght(1.5f));
+    public static final Block MUD_FURNACE = new Block(FabricBlockSettings.create().strength(1.5f));
 
 	@Override
 	public void onInitialize() {
